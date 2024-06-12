28da3c49e2701efe6cf82505c791523541080597
brndalx <113251192+brandalx@users.noreply.github.com>
2024-06-12 15:00:55 -0500

    feat: sign up layout ui

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,30 @@
-import { SignUp } from "@clerk/nextjs";
-
+import Image from "next/image";
+import { ClerkLoaded, ClerkLoading, SignUp } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center ">
+        <Image src="/logo.svg" height={100} width={100} alt="Logo" />
+      </div>
+      <div className="h-full flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16 lg:pt-0">
+          <h1 className="font-bold text-3xl text-[#2e2A47]">
+            Welcome Back to Fintrics!
+          </h1>
+          <p className="text-base text-[#7E8CA0]">
+            Log in or create an account to get back to your dashboard
+          </p>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <ClerkLoaded>
+            <SignUp path="/sign-up" />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+        </div>
+      </div>
+    </div>
+  );
 }
