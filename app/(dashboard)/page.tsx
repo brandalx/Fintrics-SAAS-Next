a5225e6bed4d82a295fa4b204cc48a9f4283a445
brndalx <113251192+brandalx@users.noreply.github.com>
2024-06-12 15:03:16 -0500

    feat: user button for protected route

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <div>This is authenticated route</div>;
+  return (
+    <div>
+      This is authenticated route
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
 }
