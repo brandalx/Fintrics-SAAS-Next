7f23981bee5909ec6122f3a169e9744f686ae540
brndalx <113251192+brandalx@users.noreply.github.com>
2024-06-12 16:16:03 -0500

    feat: books, authors routes exampel init

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,20 +4,24 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
+import authors from "./authors";
+import books from "./books";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({ error: "unauthorized" });
-  }
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+// app.get("/hello", clerkMiddleware(), (c) => {
+//   const auth = getAuth(c);
+//   if (!auth?.userId) {
+//     return c.json({ error: "unauthorized" });
+//   }
+//   return c.json({
+//     message: "Hello Next.js!",
+//   });
+// });
+
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
