943ed4ef2bf46e0ccd55c74779fcaffc2088722e
brndalx <113251192+brandalx@users.noreply.github.com>
2024-06-13 00:24:37 -0500

    feat: NavButton comp init

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,46 @@
 "use client";
 
 import React from "react";
+import { usePathname } from "next/navigation";
+import { NavButton } from "./NavButton";
+
+const routes = [
+  {
+    href: "/",
+    label: "Overview",
+  },
+  {
+    href: "/transactions",
+    label: "Transactions",
+  },
+
+  {
+    href: "/accounts",
+    label: "Accounts",
+  },
+
+  {
+    href: "/categories",
+    label: "Categories",
+  },
+  {
+    href: "/settings",
+    label: "Settings",
+  },
+];
 
 export const Navigation = () => {
-  return <div>Navigation</div>;
+  const pathname = usePathname();
+  return (
+    <nav className="hidden lg:flex items-center gap-2 overflow-x-auto">
+      {routes.map((route) => (
+        <NavButton
+          key={route.href}
+          href={route.href}
+          label={route.label}
+          isActive={pathname === route.href}
+        />
+      ))}
+    </nav>
+  );
 };
