2672afbdc6241a2b705330c5979e9548f0274486
brndalx <113251192+brandalx@users.noreply.github.com>
2024-06-13 12:01:11 -0500

    feat: sheet drawer for mobile navigation menu

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+
 type Props = {
   href: string;
   label: string;
