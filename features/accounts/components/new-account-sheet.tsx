b0900df1c2842f6dd4471ded826698e7693a586b
brndalx <113251192+brandalx@users.noreply.github.com>
2024-07-06 16:07:49 -0500

    feat: NewAccountSheet comp init

---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,13 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+const NewAccountSheet = () => {
+  return <div>new-account-sheet</div>;
+};
+
+export default NewAccountSheet;
