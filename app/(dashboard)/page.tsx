cdf099db122a9ee4ba39474289f26c8238c480a7
brndalx <113251192+brandalx@users.noreply.github.com>
2024-07-08 13:00:15 -0500

    update: styles update for sheet comp, sheet trigger button

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-accounts";
+
 export default function Home() {
-  return <div>hOME PAGE</div>;
+  const { onOpen } = useNewAccount();
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an Account</Button>
+    </div>
+  );
 }
