6e058f423af1ade8d15bcfdb6f322aafe92e0265
brndalx <113251192+brandalx@users.noreply.github.com>
2024-07-08 11:49:12 -0500

    feat: use new account hook for sheet

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useMountedState } from "react-use";
+
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 
 export const SheetProvider = () => {
+  //eq of use state - use effect => true (to fix hydration error)
+  const isMounted = useMountedState();
+
+  if (!isMounted) return null;
   return (
     <>
       <NewAccountSheet />
